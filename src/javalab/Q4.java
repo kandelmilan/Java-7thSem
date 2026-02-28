c17fffddf970d9868f1684f6bd26dd2ba3a91f4b
kandelmilan <kandelr950p@gmail.com>
2026-02-28 21:49:47 +0545

    Added

--- src/javalab/Q4.java
+++ src/javalab/Q4.java
@@ -1,22 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+// Write a Java Program to initialize and display with sum of jagged array
 package javalab;
 
 public class Q4 {
- public static void main(String[] args){
-     int[][] jarArr=new int[3][];
-     jarArr[0]=new int[]{1,2,3};
-     jarArr[1]=new int[]{4,5};
-     jarArr[2]=new int[]{6,7,8,9};
-     
-     System.out.println("Jagged Array:");
-    for(int i=0;i<3;i++){
-        for(int j=0;j<jarArr[i].length;j++){
-            System.out.print(jarArr[i][j]+" ");
+
+    public static void main(String[] args) {
+
+        int[][] jarArr = new int[3][];
+
+        // Initializing jagged array
+        jarArr[0] = new int[]{1, 2, 3};
+        jarArr[1] = new int[]{4, 5};
+        jarArr[2] = new int[]{6, 7, 8, 9};
+
+        int sum = 0;
+
+        System.out.println("Jagged Array:");
+
+        // Displaying elements and calculating sum
+        for (int i = 0; i < jarArr.length; i++) {
+            for (int j = 0; j < jarArr[i].length; j++) {
+                System.out.print(jarArr[i][j] + " ");
+                sum += jarArr[i][j];   // Adding each element to sum
+            }
+            System.out.println();
         }
-        System.out.println(" ");
+
+        System.out.println("Sum of all elements = " + sum);
     }
- }    
 }
---
+++ src/javalab/Q5.java
@@ -0,0 +1,42 @@
+/*
+Write a class Circle containing private variable radius of type float,suitable constructor , and two methoss findArea
+and findCricumference to find area and circumference of circles respectively. Write a separate class MyCircle containing 
+main methos to create and use circle objects .
+ */
+package javalab;
+// Circle.java
+class Circle {
+    
+    // Private variable
+    private float radius;
+
+    // Constructor
+    public Circle(float radius) {
+        this.radius = radius;
+    }
+
+    // Method to find Area
+    public float findArea() {
+        return (float)(Math.PI * radius * radius);
+    }
+
+    // Method to find Circumference
+    public float findCircumference() {
+        return (float)(2 * Math.PI * radius);
+    }
+}
+
+// Separate class containing main method
+public class Q5 {
+
+    public static void main(String[] args) {
+
+        // Creating Circle object
+        Circle c1 = new Circle(7);
+
+        // Displaying results
+        System.out.println("Radius: 7.0");
+        System.out.println("Area of Circle: " + c1.findArea());
+        System.out.println("Circumference of Circle: " + c1.findCircumference());
+    }
+}
