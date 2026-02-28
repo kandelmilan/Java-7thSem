c77271dc052b64b1a71d366167769b0713abe982
kandelmilan <kandelr950p@gmail.com>
2026-02-28 21:56:30 +0545

    Added

---
+++ src/javalab/Q6.java
@@ -0,0 +1,59 @@
+/*
+Write a class Distance containing private variables feet of type int and inches of type int,suitable constructor,and two methods 
+addDistance for adding and display distance objects.Write a separate class MyDistance containing main method to create and use 
+distance obejcts
+ */
+package javalab;
+// Distance class
+class Distance {
+
+    // Private variables
+    private int feet;
+    private int inches;
+
+    // Constructor
+    public Distance(int feet, int inches) {
+        this.feet = feet;
+        this.inches = inches;
+    }
+
+    // Method to add two Distance objects
+    public Distance addDistance(Distance d) {
+        int totalFeet = this.feet + d.feet;
+        int totalInches = this.inches + d.inches;
+
+        // Convert inches to feet if >= 12
+        if (totalInches >= 12) {
+            totalFeet += totalInches / 12;
+            totalInches = totalInches % 12;
+        }
+
+        return new Distance(totalFeet, totalInches);
+    }
+
+    // Method to display distance
+    public void display() {
+        System.out.println(feet + " feet " + inches + " inches");
+    }
+}
+
+// Separate class with main method
+public class Q6 {
+
+    public static void main(String[] args) {
+
+        Distance d1 = new Distance(5, 8);
+        Distance d2 = new Distance(3, 9);
+
+        System.out.print("Distance 1: ");
+        d1.display();
+
+        System.out.print("Distance 2: ");
+        d2.display();
+
+        Distance result = d1.addDistance(d2);
+
+        System.out.print("Total Distance: ");
+        result.display();
+    }
+}
---
+++ src/javalab/Q7.java
@@ -0,0 +1,36 @@
+/*
+Write a java program to implement the concept of method Overloading
+ */
+package javalab;
+
+// Adder class demonstrating Method Overloading
+class Adder {
+
+    // Method 1: Add two integers
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    // Method 2: Add three integers
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    // Method 3: Add two double values
+    double add(double a, double b) {
+        return a + b;
+    }
+}
+
+// Main class
+public class Q7 {
+
+    public static void main(String[] args) {
+
+        Adder obj = new Adder();
+
+        System.out.println("Sum of two integers: " + obj.add(10, 20));
+        System.out.println("Sum of three integers: " + obj.add(5, 10, 15));
+        System.out.println("Sum of two doubles: " + obj.add(2.5, 3.5));
+    }
+}
