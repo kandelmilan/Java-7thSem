601200c06ebe7d651510fabba7a9481df365f359
kandelmilan <kandelr950p@gmail.com>
2026-03-13 17:13:25 +0545

    Added

--- src/Lab3/lab3002.java
+++ src/Lab3/lab3002.java
@@ -1,6 +1,6 @@
 /*
 Write a Gui application to calculate simple interest.Use three text fields for input and fourth text fields for output.
-Your preogram should displat simple interest if the user press the mouse and compound interest if user releases the mouse.
+Your program should display simple interest if the user press the mouse and compound interest if user releases the mouse.
 */
 
 package Lab3;
@@ -24,17 +24,15 @@ public class lab3002 implements MouseListener {
         l3 = new JLabel("Time:");
         l4 = new JLabel("Result:");
 
-        t1 = new JTextField(15);
-        t2 = new JTextField(15);
-        t3 = new JTextField(15);
-        t4 = new JTextField(15);
+        t1 = new JTextField();
+        t2 = new JTextField();
+        t3 = new JTextField();
+        t4 = new JTextField();
 
         b = new JButton("Calculate");
 
         f.setSize(350,300);
         f.setLayout(null);
-        f.setVisible(true);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         l1.setBounds(30,30,100,20);
         t1.setBounds(150,30,120,20);
@@ -42,13 +40,13 @@ public class lab3002 implements MouseListener {
         l2.setBounds(30,70,100,20);
         t2.setBounds(150,70,120,20);
 
-        l3.setBounds(30,110,100,20);
-        t3.setBounds(150,110,120,20);
+       l3.setBounds(30,110,100,20);
+t3.setBounds(150,110,120,20);
 
-        l4.setBounds(30,150,100,20);
-        t4.setBounds(150,150,120,20);
+b.setBounds(100,180,120,30);
 
-        b.setBounds(100,200,120,30);
+l4.setBounds(30,230,150,20);
+t4.setBounds(150,230,120,20);
 
         f.add(l1); f.add(t1);
         f.add(l2); f.add(t2);
@@ -57,27 +55,32 @@ public class lab3002 implements MouseListener {
         f.add(b);
 
         b.addMouseListener(this);
+
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    public void mousePressed(MouseEvent e){
-        double p = Double.parseDouble(t1.getText());
-        double r = Double.parseDouble(t2.getText());
-        double t = Double.parseDouble(t3.getText());
+   public void mousePressed(MouseEvent e){
+    double p = Double.parseDouble(t1.getText());
+    double r = Double.parseDouble(t2.getText());
+    double t = Double.parseDouble(t3.getText());
 
-        double si = (p*r*t)/100;
+    double si = (p*r*t)/100;
 
-        t4.setText(String.valueOf(si));
-    }
+    l4.setText("Simple Interest:");
+    t4.setText(String.valueOf(si));
+}
 
-    public void mouseReleased(MouseEvent e){
-        double p = Double.parseDouble(t1.getText());
-        double r = Double.parseDouble(t2.getText());
-        double t = Double.parseDouble(t3.getText());
-        
-        double ci = p * Math.pow((1 + r/100), t) - p;
+public void mouseReleased(MouseEvent e){
+    double p = Double.parseDouble(t1.getText());
+    double r = Double.parseDouble(t2.getText());
+    double t = Double.parseDouble(t3.getText());
 
-        t4.setText(String.valueOf(ci));
-    }
+    double ci = p * Math.pow((1 + r/100), t) - p;
+
+    l4.setText("Compound Interest:");
+    t4.setText(String.valueOf(ci));
+}
 
     public void mouseClicked(MouseEvent e){}
     public void mouseEntered(MouseEvent e){}
