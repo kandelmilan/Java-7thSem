677e69b4f74b5934c76763171d96ef51e3e90a7a
kandelmilan <kandelr950p@gmail.com>
2026-03-16 20:15:56 +0545

    Added

---
+++ src/lab4/Q40001.java
@@ -0,0 +1,11 @@
+/* 
+Design a GUI form using swing with a text field , a text label for displaying the inout message "Input any String" and threen
+buttons with caption CheckPlaindrome,Reverse,adn FindVowels. Write a complete program for above scenario and for checking palindrome in first button,
+reverse it after clicking second button and extract vowel from it after clicking third button.
+
+*/
+package lab4;
+
+public class Q40001 {
+    
+}
