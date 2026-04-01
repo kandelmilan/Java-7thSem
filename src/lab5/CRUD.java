4fdf159de60eb81fb39e30ac2a92521a9a81fbfb
kandelmilan <kandelr950p@gmail.com>
2026-04-01 15:40:22 +0545

    Added

---
+++ src/lab5/CRUD.java
@@ -0,0 +1,13 @@
+/*
+Write a java console program to perform CRUD operation on database [Database :Company Table:
+Employee (Eid,Nmae,Address,And Salary)]
+ */
+package lab5;
+
+/**
+ *
+ * @author kandelmilan
+ */
+public class CRUD {
+    
+}
